b879cbf7354f09f9c482656950d642c959208f57
TheGamersCave <asydix@gmail.com>
2015-04-21 20:29:15 -0300

    Simple push, for sake of keeping project.

--- src/main/java/com/caved_in/commonsexamples/minigame/MiniGameExample.java
+++ src/main/java/com/caved_in/commonsexamples/minigame/MiniGameExample.java
@@ -1,8 +1,38 @@
 package com.caved_in.commonsexamples.minigame;
 
 import com.caved_in.commons.game.MiniGame;
+import com.caved_in.commonsexamples.minigame.users.GamePlayerManager;
 
-public class MiniGameExample extends MiniGame<> {
+public class MiniGameExample extends MiniGame<GamePlayerManager> {
+    @Override
+    public void startup() {
+
+    }
+
+    @Override
+    public void shutdown() {
+
+    }
+
+    @Override
+    public String getVersion() {
+        return null;
+    }
+
+    @Override
+    public String getAuthor() {
+        return null;
+    }
+
+    @Override
+    public void initConfig() {
+
+    }
+
+    @Override
+    public long tickDelay() {
+        return 0;
+    }
     /*
     This example will be completed in the next patch, displaying a simple TAG Minigame!
      */
--- src/main/java/com/caved_in/commonsexamples/minigame/users/GamePlayerManager.java
+++ src/main/java/com/caved_in/commonsexamples/minigame/users/GamePlayerManager.java
@@ -2,5 +2,5 @@ package com.caved_in.commonsexamples.minigame.users;
 
 import com.caved_in.commons.game.players.UserManager;
 
-public class GamePlayerManager extends UserManager<> {
+public class GamePlayerManager extends UserManager<GamePlayer> {
 }
